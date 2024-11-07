88d0746f829fb1e77392f57e7378533f4340500d
Cahangir <esgerovcahangir12@gmail.com>
2024-11-07 13:02:37 +0400

    New custom styles will be added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
+import { Routes } from "react-router-dom";
 const App = () => {
-  return <></>;
+  return (
+    <div className="primaryBgColor defaultHeight">
+      <div className="mx-[10%]">
+        <Routes></Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
